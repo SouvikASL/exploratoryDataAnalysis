45729abef37368471418c20fea57525542c7dd6a
SouvikASL <souvik.17@gmail.com>
2015-05-11 01:09:05 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,15 @@
-# exploratoryDataAnalysis
+householdData <- read.table("household_power_consumption.txt", header=T, sep=";")
+householdData$Date <- as.Date(householdData$Date, format="%d/%m/%Y")
+df <- householdData[(householdData$Date=="2007-02-01") | (householdData$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+
+hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
